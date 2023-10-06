0b4f34fd8f1db979cbb0227bb7b54f5b0a2a1b4b
Tavoleglise <tavoleglise@gmail.com>
2023-10-06 14:16:45 -0600

    px3000 page implemented and merged, bugs main page fixed

--- px3000/xeicon.js
+++ px3000/xeicon.js
@@ -1,8 +1,43 @@
+const navbuttons_group = document.querySelectorAll(".menu-container ul li");
 const button_to_contact = document.getElementById("button_to_contact");
+
+const navbar_icon = document.querySelector(".burguermenu-icon");
+const burguermenu = document.querySelector(".burguermenu");
 
 button_to_contact.addEventListener("click", () => {
   gsap.to(window, {
     duration: 1,
-    scrollTo: `#footer`,
+    scrollTo: `#contacto`,
   });
 });
+
+navbuttons_group.forEach((button, index) => {
+  if (index === 0) {
+    return;
+  }
+  button.addEventListener("click", () => {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: `${button.getAttribute("href")}`,
+    });
+  });
+});
+
+//-------------------------------------------------------------------------------------
+let isMenuHidden = false;
+
+const menuDisplay = (flag) => {
+  gsap.to(burguermenu, {
+    display: `${flag ? "none" : "block"}`,
+  });
+  gsap.to(burguermenu, {
+    opacity: `${flag ? 0 : 1}`,
+  });
+};
+navbar_icon.addEventListener("click", () => {
+  menuDisplay(isMenuHidden);
+  isMenuHidden = !isMenuHidden;
+});
+console.log(isMenuHidden);
+
+//-------------------------------------------------------------------------------------
